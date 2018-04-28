1a809e5e60b8df7f1d9e2b35ebaa2f0474eacf21
petrec <constantinpetre89@gmail.com>
2018-04-28 15:03:29 +0300

    reserv

--- src/com/sda/hotel/Reservation.java
+++ src/com/sda/hotel/Reservation.java
@@ -1,19 +1,26 @@
 package com.sda.hotel;
-import java.util.Date;
+import java.util.Calendar;
 
 public class Reservation {
 
-    private Date checkIn, checkOut;
+    private Calendar checkIn, checkOut;
     private Client client;
 
-    public Reservation(Date checkIn, Date checkOut, Client client){
+    public Reservation(Calendar checkIn, Calendar checkOut, Client client){
         this.checkIn = checkIn;
         this.checkOut = checkOut;
         this.client = client;
     }
 
-    //addReservation
-    checkAvailability
-    cancelReservation
+    public Calendar getCheckIn() {
+        return checkIn;
+    }
 
+    public Calendar getCheckOut() {
+        return checkOut;
+    }
+
+    public Client getClient() {
+        return client;
+    }
 }
